bc59a2136372c6b69f5a801d4d0a524267698c01
Ryan Stegmann <uctestacc1@gmail.com>
2018-08-21 14:37:09 -0700

    Add login thunk

--- src/pages/Login/actions/login.js
+++ src/pages/Login/actions/login.js
@@ -1,11 +1,12 @@
-export const onLoginSubmit = ({username, password}) =>
-({
-  type: 'LOGIN_SUBMIT',
-  payload: {
-    username,
-    password
-  }
-})
+import su from 'superagent'
+
+export const onLoginSubmit = ({username, password}) => dispatch => {
+  dispatch({ type: 'LOGIN_SUBMIT' })
+  su.post('/api/v1/login')
+    .send({ username, password })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
 
 export const onLoginSuccess = res =>
 ({
