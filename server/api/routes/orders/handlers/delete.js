66f2a95af5d0b57e11bb0ee4c6cc6d02dc93a20c
Ryan Stegmann <uctestacc1@gmail.com>
2018-09-01 14:51:39 -0700

    Update to set canceled instead of deleting

--- server/api/routes/orders/handlers/delete.js
+++ server/api/routes/orders/handlers/delete.js
@@ -1,7 +1,7 @@
 const { Order } = requireDb
 
 module.exports = (req, res) =>
-  Order.destroy({ where: { uid: req.body.order.uid, userId: req.user.id } })
+  Order.update({ status: 'canceled' }, { where: { uid: req.body.order.uid, userId: req.user.id } })
     .then(order => {
       res.status(200).json({ order })
     })
