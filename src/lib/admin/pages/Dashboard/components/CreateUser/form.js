fd076cef1d741e03ba290558c5bed3fa4d7085d0
Ryan Stegmann <uctestacc1@gmail.com>
2018-09-23 11:19:43 -0700

    Change user type to radio buttons for admin

--- src/lib/admin/pages/Dashboard/components/CreateUser/form.js
+++ src/lib/admin/pages/Dashboard/components/CreateUser/form.js
@@ -4,7 +4,7 @@ import { Field, reduxForm } from 'redux-form'
 
 import { validate, asyncValidate } from './validators'
 
-import { Alert, Form, Button, Input, Icon } from 'antd'
+import { Radio, Alert, Form, Button, Input, Icon } from 'antd'
 
 const FormItem = Form.Item
 
@@ -16,13 +16,21 @@ const InputField = ({ input, meta, icon, ...rest }) =>
       {...rest} />
   </FormItem>
 
+const UserTypeField = ({ input, meta, icon, ...rest }) =>
+  <FormItem hasFeedback validateStatus={meta.touched ? (meta.error ? 'error' : 'success') : ''} help={meta.touched && meta.error ? meta.error : ''}>
+    <Radio.Group {...input} {...rest} defaultValue="individual" buttonStyle="solid">
+      <Radio.Button value="individual">User</Radio.Button>
+      <Radio.Button value="designer">Designer</Radio.Button>
+    </Radio.Group>
+  </FormItem>
+
 const CreateUserForm = ({error, handleSubmit, submitting}) =>
   <Form onSubmit={handleSubmit}>
     <Field component={InputField} name="email" type="email" icon={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Email' />
     <Field component={InputField} name="name" type="text" icon={<Icon type="idcard" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Full name' />
     <Field component={InputField} name="password" type="password" icon={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Password' />
     <Field component={InputField} name="repeat" type="password" icon={<Icon type="reload" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Verify Password' />
-    <Field component={InputField} name="userType" type="text" icon={<Icon type="reload" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='individual/designer/admin' />
+    <Field component={UserTypeField} name="userType" type="text" icon={<Icon type="reload" style={{ color: 'rgba(0,0,0,.25)' }} />} />
     {error && <Alert message={error} type="error" closable style={{ marginBottom: '20px' }} />}
   </Form>
 
